8db280f9370963f4315c749c40d84d797e4e4473
Mark Robertson <markrivasrobertson@yahoo.com>
2016-09-25 20:29:19 -0400

    begin setting up userview on individual user routes.  Hook up the route to a given user (identified by an uid number) and test

---
+++ src/components/user_view.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import request from 'superagent';
+
+class UserView extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>A User View beginning</h1>
+      </div>
+    );
+  }
+}
+
+export default UserView;
--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
-import request from 'superagent';
 import Main from '../components/main.jsx';
 import Home from '../components/home.jsx';
 import Login from '../components/login.jsx';
 import GroupView from '../components/group_view.jsx';
-// import UserView from '../components/user_view.jsx';
+import UserView from '../components/user_view.jsx';
 // import NewPhoto from '../components/new_photo.jsx';
 // import Photo from '../components/photo.jsx';
 import requireAuth from '../utils/auth.js';
@@ -50,6 +49,7 @@ class Routes extends React.Component {
           <IndexRoute component={Home} />
           <Route path="login" component={Login} />
           <Route path="/users" component={GroupView} onEnter={requireAuth} />
+          <Route path="/users/:user" component={UserView} onEnter={requireAuth} />
         </Route>
       </Router>
     );
