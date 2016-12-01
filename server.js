d036f9cc2a0c26a58531b926666aafc646c7a5de
Mark Robertson <markrivasrobertson@yahoo.com>
2016-12-01 17:28:33 -0500

    debug heroku launch.  Server.js adjustments based off previous project server file

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.set('port', (process.env.PORT || 5000));
+// app.set('port', (process.env.PORT || 5000));
 
-// app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/src' + '/static');
-app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('/index');
+  // response.render('/index');
+  response.sendFile(path.join(__dirname, 'src/static/index.html'));
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Listening on Port: ${port}`);
 });
